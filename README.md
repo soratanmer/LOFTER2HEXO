70911e149393246c0f04ee315efe7c42a9622d3a
soratanmer <soratanmer@gmail.com>
2020-06-14 22:02:43 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # LOFTER2HEXO
- 将 LOFTER 导出的 XML 文件转换为 Markdown 文件工具
+一个将 LOFTER 导出的 XML 文件转换为适合 Hexo 解析的 Markdown 文件工具。
+
+项目地址：https://github.com/soratanmer/LOFTER2HEXO
+
+此项目使用[LOFTER2Hexo](https://github.com/boboidream/LOFTER2Hexo)和[lofter2jekyll](https://github.com/FromEndWorld/lofter2jekyll/)修改而成。
+
+## 
+
+## 
+
+## 使用说明
+
+### 环境配置
+
+### 
+
+node.js : 如需安装请查看 [Node.js 下载](https://nodejs.org/en/download/)。
+
+### 
+
+### 使用程序
+
+1. 将LOFTER导出的XML文件命名为`LOFTER.xml`并移至此项目所在文件夹。
+
+2. 运行以下命令：
+
+   ```powershell
+   npm install
+   ```
+
+   ```powershell
+   npm run start
+   ```
+
+### 运行结果
+
+1. 在 `Terminal` 中会打印日志。
+2. 生成 `LOFTER` 文件夹，包含所有 Markdown 文件；所有图片下载到 `LOFTER/img` 文件夹下。
+
+### 参数说明
+
+```powershell
+Usage: npm run start [options]
+Options:
+    -h, --help           output usage information
+    -V, --version        output the version number
+    -i, --input <lang>   xml 文件路径，例如：`/Volumes/私人/Github/test.xml`
+    -n, --notag          头部不生成 tags 标签（以避免生成太多 `Tags` 造成的不美观）
+    -j, --jekyll         导出 jekyll 格式 Markdown 文件
+    -a, --author <lang>  设置 jekyll 格式 Markdown 头部 author
+```
+
+命令举例
+
+```powershell
+npm run start -i ./Github/test.xml -n 
+# 解析路径为 `./Github/test.xml` 文件，生成 Markdown 文件不带 Tags 标签
+npm run start -n -j -a soratanmer 
+# 解析当前目录下 LOFTER.xml 文件，生成不带 Tags 标签的 Jekyll 格式 Markdown 文件，头部 author: soratanmer
+```
+
+## 其他选项
+
+另有功能类似的脚本[lofter2Hexo](https://github.com/alicewish/Lofter2Hexo)大家可以试试。
+
+墨问太太的脚本基于Python，这个基于JS。
+
+这个的优点大概是下载图片比较利索，直接根据图片URL下载并且链接到md文档中。
+
+缺点是没有图形界面，需要安装node.js和使用命令行，还有它不能直接将导出内容再发布。
---
+++ index.js
@@ -0,0 +1,188 @@
+/*
+ * @author       : soratanmer
+ * @Github       : https://github.com/soratanmer
+ * @Date         : 2020-06-14 21:10:34
+ * @LastEditors  : soratanmer
+ * @LastEditTime : 2020-06-14 21:28:57
+ * @FilePath     : \LOFTER2HEXO\index.js
+ * @Description  : 将 LOFTER 导出的 XML 文件转换为 Markdown 文件
+ */
+
+const fs = require('fs')
+const xml2js = require('xml2js')
+const toMarkdown = require('to-markdown')
+const argv = require('commander')
+const path = require('path')
+const imageDownloader = require('image-downloader')
+const cwd = process.cwd()
+const parser = new xml2js.Parser()
+
+argv
+  .version('1.0.0')
+  .usage('[options]')
+  .option('-i --input <lang>', 'lofter xml file')
+  .option('-n, --notag', 'Without tags')
+  .option('-j, --jekyll', 'jekyll type')
+  .option('-a, --author <lang>', 'set author in header in jekyll type')
+  .parse(process.argv)
+
+class Lofter2md {
+  constructor() {
+    this.outputFilePath = path.resolve(cwd, 'LOFTER')
+    this.outputImgPath = path.resolve(cwd, 'LOFTER/img')
+    this.file = argv.input || path.resolve(cwd, 'LOFTER.xml')
+    this.author = argv.author || ''
+  }
+  run() {
+    if (!fs.existsSync(this.outputFilePath)) {
+      fs.mkdirSync(this.outputFilePath)
+    }
+    if (!fs.existsSync(this.outputImgPath)) {
+      fs.mkdirSync(this.outputImgPath)
+    }
+    this.getPostArray(this.file, (postArray) => {
+      this.parsePost(postArray)
+    })
+  }
+  getPostArray(file, callback) {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        console.error(`读取文件 ${file} 出错：`, err)
+        return
+      }
+      parser.parseString(data, (error, result) => {
+        callback(result.lofterBlogExport.PostItem)
+      })
+    })
+  }
+  parsePost(postArray) {
+    postArray.forEach((article, index) => {
+      const newDate = new Date(parseInt(article.publishTime)).Format(
+        'yy-MM-dd hh:mm:ss'
+      )
+      let fileName =
+        newDate.substring(0, 10) + '-' + article.title + '-' + ++index + '.md'
+      const allWord = this.parseHeader(article) + this.parseContent(article)
+      if (fileName.indexOf('/') != null) {
+        fileName = fileName.replace(/\//g, '_')
+      }
+      if (fileName.indexOf('*') != null) {
+        fileName = fileName.replace(/\*/g, '_')
+      }
+      if (fileName.indexOf('?') != null) {
+        fileName = fileName.replace(/\?/g, '_')
+      }
+      this.createMD(fileName, allWord, index)
+    })
+  }
+  createMD(fileName, allWord, index) {
+    fs.open(path.resolve(this.outputFilePath, fileName), 'w', (err) => {
+      if (err) {
+        throw err
+      }
+      fs.writeFile(
+        path.resolve(this.outputFilePath, fileName),
+        allWord,
+        (err) => {
+          if (err) {
+            throw err
+          }
+          console.log(`${index}. Create ${fileName} successfully!`)
+        }
+      )
+    })
+  }
+  parseHeader(article) {
+    const tags = argv.notag ? '' : article.tag
+    const newDate = new Date(parseInt(article.publishTime)).Format(
+      'yyyy-MM-dd hh:mm:ss'
+    )
+    let headline = ''
+    if (argv.author) {
+      const res = ''
+      if (tags) {
+        tags.forEach((tag) => {
+          res += '    - ' + tag + '\n'
+        })
+      }
+      headline = `---\nlayout: post\ntitle: ${article.title}\ndate: ${newDate}\nauthor: ${this.author}\ncatalog: 随笔\ntags: [${res}]\n\n---\n`
+    } else {
+      headline = `---\nlayout: post\ntitle: ${article.title}\ndate: ${newDate}\ntags: [${tags}]\n\n---\n`
+    }
+    return headline
+  }
+  parseContent(article) {
+    let content = ''
+    let contents = ''
+    let imgArray = []
+    if (article.content) {
+      contents = toMarkdown(article.content.toString())
+      content = contents.replace(/!\[.*?\]\((.*?)\)/g, '')
+      imgArray = contents.match(/!\[.*?\]\((.*?)\)/g)
+      if (imgArray && imgArray.length) {
+        imgArray.forEach((imgURL) => {
+          imgURL = imgURL.match(/http.*\.(jpg|jpeg|gif|png)/)[0]
+          const imgName = imgURL.split('/').pop()
+          content += `\n![图片](./img/${imgName})\n`
+          this.downloadImg(imgURL)
+        })
+      }
+    } else if (article.photoLinks != null) {
+      content = toMarkdown(article.caption.toString()) + '\n\n'
+      const text = article.photoLinks[0]
+      imgArray = JSON.parse(text)
+      imgArray.forEach((img) => {
+        const imgURL = img.orign.split('?')[0]
+        const imgName = imgURL.split('/').pop()
+        content += `\n![图片](./img/${imgName})\n`
+        this.downloadImg(imgURL)
+      })
+    } else if (article.caption != null) {
+      content = toMarkdown(article.caption.toString())
+    } else {
+      console.log(article)
+    }
+    return content
+  }
+  downloadImg(imgURL) {
+    imageDownloader
+      .image({
+        url: imgURL,
+        dest: this.outputImgPath,
+      })
+      .then(({ filename }) => {
+        console.log('Image save to', filename)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+}
+
+Date.prototype.Format = function (fmt) {
+  const o = {
+    'M+': this.getMonth() + 1,
+    'd+': this.getDate(),
+    'h+': this.getHours(),
+    'm+': this.getMinutes(),
+    's+': this.getSeconds(),
+    'q+': Math.floor((this.getMonth() + 3) / 3),
+    S: this.getMilliseconds(),
+  }
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  for (const k in o)
+    if (new RegExp('(' + k + ')').test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+  return fmt
+}
+
+const lofter = new Lofter2md()
+
+lofter.run()
